c05bbb29d3147951b3e7ef34baeb8e156b227cc0
empty-works <pama.mark@gmail.com>
2019-12-09 21:12:56 +0100

    Project start.

---
+++ FTPClientInJava/src/ftpclientinjava/FTPClientInJava.java
@@ -0,0 +1,18 @@
+/*
+ */
+package ftpclientinjava;
+
+/**
+ *
+ * @author MP
+ */
+public class FTPClientInJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
