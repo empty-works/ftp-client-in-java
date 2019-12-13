ad69b1f537ff9d1b405193cf50cc346d15990da2
empty-works <pama.mark@gmail.com>
2019-12-13 22:01:52 +0100

    Switched back to a GUI interface. No longer launched from a shell.

--- FTPClientInJava/src/ftpclientinjava/Main.java
+++ FTPClientInJava/src/ftpclientinjava/Main.java
@@ -11,9 +11,8 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         
-        
-    }
-    
+    //    System.out.println("Enter ");
+    //}
 }
