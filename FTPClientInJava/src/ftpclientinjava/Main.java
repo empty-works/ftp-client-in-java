9a60555bbaa750bb5339fc39c963137524692cb3
empty-works <pama.mark@gmail.com>
2019-12-13 21:32:52 +0100

    The app is no longer launched as a GUI. It is now just launched from the command line for now from the Main class.

---
+++ FTPClientInJava/src/ftpclientinjava/Main.java
@@ -0,0 +1,19 @@
+/*
+ */
+package ftpclientinjava;
+
+/**
+ *
+ * @author MP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
