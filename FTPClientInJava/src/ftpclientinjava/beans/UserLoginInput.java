b850b11b38af3d56382795d8e1a0bdad8b8d4fdb
empty-works <pama.mark@gmail.com>
2019-12-15 00:32:21 +0100

    UserLoginInput stores the user's input. Also, a connect button has been added to the UI.

---
+++ FTPClientInJava/src/ftpclientinjava/beans/UserLoginInput.java
@@ -0,0 +1,15 @@
+/*
+ */
+package ftpclientinjava.beans;
+
+/**
+ *
+ * @author MP
+ */
+public class UserLoginInput {
+    
+    public String server;
+    public String username;
+    public String password;
+    public int port;
+}
--- FTPClientInJava/src/ftpclientinjava/ui/LoginUi.java
+++ FTPClientInJava/src/ftpclientinjava/ui/LoginUi.java
@@ -27,8 +27,13 @@ public class LoginUi extends javax.swing.JPanel {
         UsernameLabel = new javax.swing.JLabel();
         UsernameTextField1 = new javax.swing.JTextField();
         LogCon3 = new javax.swing.JPanel();
+        PasswordLabel = new javax.swing.JLabel();
+        PasswordField = new javax.swing.JPasswordField();
         LogCon4 = new javax.swing.JPanel();
+        PortLabel = new javax.swing.JLabel();
+        PortTextField = new javax.swing.JTextField();
         LogCon5 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
 
         setToolTipText(null);
         setLayout(new java.awt.GridLayout(1, 5));
@@ -47,6 +52,7 @@ public class LoginUi extends javax.swing.JPanel {
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 0);
         LogCon1.add(ServerLabel, gridBagConstraints);
 
         ServerTextField.setToolTipText(null);
@@ -77,6 +83,7 @@ public class LoginUi extends javax.swing.JPanel {
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 0);
         LogCon2.add(UsernameLabel, gridBagConstraints);
 
         UsernameTextField1.setToolTipText(null);
@@ -96,16 +103,73 @@ public class LoginUi extends javax.swing.JPanel {
         LogCon3.setToolTipText(null);
         LogCon3.setOpaque(false);
         LogCon3.setLayout(new java.awt.GridBagLayout());
+
+        PasswordLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        PasswordLabel.setText("Password");
+        PasswordLabel.setToolTipText(null);
+        PasswordLabel.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        PasswordLabel.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 0);
+        LogCon3.add(PasswordLabel, gridBagConstraints);
+
+        PasswordField.setToolTipText(null);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.weighty = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 5, 5);
+        LogCon3.add(PasswordField, gridBagConstraints);
+
         add(LogCon3);
 
         LogCon4.setToolTipText(null);
         LogCon4.setOpaque(false);
         LogCon4.setLayout(new java.awt.GridBagLayout());
+
+        PortLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        PortLabel.setText("Port:");
+        PortLabel.setToolTipText(null);
+        PortLabel.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        PortLabel.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 0);
+        LogCon4.add(PortLabel, gridBagConstraints);
+
+        PortTextField.setToolTipText(null);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.weighty = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 5, 100);
+        LogCon4.add(PortTextField, gridBagConstraints);
+
         add(LogCon4);
 
         LogCon5.setToolTipText(null);
         LogCon5.setOpaque(false);
         LogCon5.setLayout(new java.awt.GridBagLayout());
+
+        jButton1.setText("Connect");
+        jButton1.setToolTipText(null);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(0, 30, 0, 30);
+        LogCon5.add(jButton1, gridBagConstraints);
+
         add(LogCon5);
     }// </editor-fold>//GEN-END:initComponents
 
@@ -116,9 +180,14 @@ public class LoginUi extends javax.swing.JPanel {
     private javax.swing.JPanel LogCon3;
     private javax.swing.JPanel LogCon4;
     private javax.swing.JPanel LogCon5;
+    private javax.swing.JPasswordField PasswordField;
+    private javax.swing.JLabel PasswordLabel;
+    private javax.swing.JLabel PortLabel;
+    private javax.swing.JTextField PortTextField;
     private javax.swing.JLabel ServerLabel;
     private javax.swing.JTextField ServerTextField;
     private javax.swing.JLabel UsernameLabel;
     private javax.swing.JTextField UsernameTextField1;
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
