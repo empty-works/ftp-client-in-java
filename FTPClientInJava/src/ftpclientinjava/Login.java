b9eeec0b25613ec604fd97edce60e8b997e93705
empty-works <pama.mark@gmail.com>
2019-12-16 18:09:20 +0100

    Login now has getters and setters for server, username, password, and port number.

--- FTPClientInJava/src/ftpclientinjava/Login.java
+++ FTPClientInJava/src/ftpclientinjava/Login.java
@@ -11,14 +11,79 @@ import ftpclientinjava.beans.UserLoginInput;
 public class Login {
     
     private UserLoginInput userLogin;
+    private String server;
+    private String username;
+    private String password;
+    private int port;
     
     public Login(UserLoginInput userLogin) {
         
         this.userLogin = userLogin;
     }
-    
-    public void setServer() {
+
+    /**
+     * @return the server
+     */
+    public String getServer() {
         
-        
+        return server;
     }
+
+    /**
+     * @param server the server to set
+     */
+    public void setServer(String server) {
+        
+        this.server = server;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        
+        this.password = password;
+    }
+
+    /**
+     * @return the port
+     */
+    public int getPort() {
+        
+        return port;
+    }
+
+    /**
+     * @param port the port to set
+     */
+    public void setPort(int port) {
+        
+        this.port = port;
+    }
+    
+    
 }
