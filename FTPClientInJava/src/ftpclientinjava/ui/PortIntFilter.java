e023d2ce38621c945dc6400595d870d1cb71bf57
empty-works <pama.mark@gmail.com>
2019-12-17 20:58:15 +0100

    The insertString method in PortIntFilter simply adds the string input by the user to a StringBuilder object to test it as an integer.

--- FTPClientInJava/src/ftpclientinjava/ui/PortIntFilter.java
+++ FTPClientInJava/src/ftpclientinjava/ui/PortIntFilter.java
@@ -2,11 +2,26 @@
  */
 package ftpclientinjava.ui;
 
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
+
 /**
  *
  * @author MP
  */
-public class PortIntFilter {
+public class PortIntFilter extends DocumentFilter {
     
-    
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string, 
+            AttributeSet attr) throws BadLocationException {
+        
+        Document doc = fb.getDocument();
+        StringBuilder sb = new StringBuilder();
+        sb.append(doc.getText(0, doc.getLength()));
+        sb.insert(offset, string);
+        
+        
+    }
 }
