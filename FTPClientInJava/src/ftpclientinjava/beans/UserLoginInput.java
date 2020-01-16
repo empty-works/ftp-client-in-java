7aad59537bcf18307ff573d68e6ab5abb5635f98
empty-works <pama.mark@gmail.com>
2020-01-16 23:58:44 +0100

    Set getters and setters for login variables in UserLoginInput.

--- FTPClientInJava/src/ftpclientinjava/beans/UserLoginInput.java
+++ FTPClientInJava/src/ftpclientinjava/beans/UserLoginInput.java
@@ -12,4 +12,36 @@ public class UserLoginInput {
     public String username;
     public String password;
     public int port;
+    
+    public void setServer(String server) {
+        this.server = server;
+    }
+    
+    public String getServer() {
+        return server;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
+    public int getPort() {
+        return port;
+    }
 }
