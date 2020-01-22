13dcbffabf72a3ae73d049a0950240572ccab9ff
empty-works <pama.mark@gmail.com>
2020-01-22 14:50:22 +0100

    Removed all getters within FtpClientHandler.

--- FTPClientInJava/src/ftpclientinjava/utilities/FtpClientHandler.java
+++ FTPClientInJava/src/ftpclientinjava/utilities/FtpClientHandler.java
@@ -12,6 +12,7 @@ public class FtpClientHandler {
     private String username;
     private String password;
     private int port;
+    private 
     
     private static FtpClientHandler login_instance = null;
     
@@ -34,36 +35,22 @@ public class FtpClientHandler {
         this.server = server;
     }
     
-    public String getServer() {
-        return server;
-    }
-    
     public void setUsername(String username) {
         this.username = username;
-    }
-    
-    public String getUsername() {
-        return username;
     }
     
     public void setPassword(String password) {
         this.password = password;
     }
     
-    public String getPassword() {
-        return password;
-    }
-    
     public void setPort(int port) {
         this.port = port;
-    }
-    
-    public int getPort() {
-        return port;
     }
     
     public void login() {
         
         
     }
+    
+    private void 
 }
