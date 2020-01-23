443296c159c53b1f323e159f4c63264911151da1
empty-works <pama.mark@gmail.com>
2020-01-23 15:14:46 +0100

    Deleting obsolete code such as unused imports and code that directly made use of the Downloader class. The interface is now FtpClientHandler.

--- FTPClientInJava/test/ftpclientinjava/DownloaderTest.java
+++ FTPClientInJava/test/ftpclientinjava/DownloaderTest.java
@@ -7,7 +7,6 @@ import ftpclientinjava.unit_test.FakeFtpServerCreator;
 import ftpclientinjava.utilities.FtpClientHandler;
 import java.io.File;
 import java.io.IOException;
-import org.apache.commons.net.ftp.FTPClient;
 import static org.hamcrest.io.FileMatchers.anExistingFile;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -23,7 +22,6 @@ import org.mockftpserver.fake.FakeFtpServer;
  */
 public class DownloaderTest {
     
-    private FTPClient ftpclient;
     private FakeFtpServer fakeFtpServer;
     private FtpClientHandler ftpClient;
     
@@ -48,12 +46,6 @@ public class DownloaderTest {
         ftpClient.setPassword(TestFtpServerLogin.PASSWORD);
         ftpClient.setServer("localhost");
         ftpClient.setPort(fakeFtpServer.getServerControlPort());
-        
-        /*
-        Connection con = new Connection(
-                TestFtpServerLogin.USER, TestFtpServerLogin.PASSWORD, "localhost", port);
-        ftpclient = con.getFTPClient();
-        */
     }
     
     @After
@@ -70,9 +62,6 @@ public class DownloaderTest {
         String destination = "downloaded_test001.txt";
         ftpClient.downloadFile(source, destination);
         ftpClient.disconnect();
-        //Downloader instance = new Downloader(ftpclient);
-        //instance.downloadFile(source, destination);
-        ftpclient.disconnect();
         fakeFtpServer.stop();
         assertThat(new File(destination), anExistingFile());
     }
